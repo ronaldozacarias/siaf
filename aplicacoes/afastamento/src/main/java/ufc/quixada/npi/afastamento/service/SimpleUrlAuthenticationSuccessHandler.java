fe16fa07694a2df4de11d054dbf6d5cfe02f5591
Virginia <virginia.farias7@gmail.com>
2015-02-23 17:11:20 -0300

    Alteração na autenticação pra permitir apenas perfis de usuário do sistema: admin e docente.

---
+++ aplicacoes/afastamento/src/main/java/ufc/quixada/npi/afastamento/service/SimpleUrlAuthenticationSuccessHandler.java
@@ -0,0 +1,38 @@
+package ufc.quixada.npi.afastamento.service;
+
+import java.io.IOException;
+import java.util.Collection;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.RedirectStrategy;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import br.ufc.quixada.npi.ldap.model.Constants;
+
+public class SimpleUrlAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+	
+	private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+
+	@Override
+	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+		Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+		boolean usuarioValido = false;
+        for (GrantedAuthority grantedAuthority : authorities) {
+            if (grantedAuthority.getAuthority().equals(Constants.AFFILIATION_DOCENTE) || grantedAuthority.getAuthority().equals(Constants.AFFILIATION_ADMIN_SIAF)) {
+            	usuarioValido = true;
+                break;
+            }
+        }
+        if(!usuarioValido) {
+        	redirectStrategy.sendRedirect(request, response, "/loginfailed");
+        }
+		
+	}
+
+}
